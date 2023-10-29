81cd51e669cc0ecbf8bfde7343337c0fe7896460
Juniper Parsons <juniperparsnips@gmail.com>
2023-10-29 15:35:26 -0400

    Remove `Agent` in favor of `InteractsWith`

--- src/interactions.rs
+++ src/interactions.rs
@@ -4,30 +4,21 @@ use bevy::{
 };
 
 /// Self and `Target` must be mutually exclusive in order to satisfy rust's borrow checker
-pub trait InteractsWith<Target: Component>: Component {
+pub trait InteractsWith<Target: Component>
+where
+    Self: Component + Sized,
+{
     type SelfInfo: WorldQuery;
     type TargetInfo: WorldQuery;
-    // where
-    //     &'a mut Self::SelfInfo: WorldQuery<Item<'a> = Self::SelfInfo>;
-
-    /// Whether or not this component should even try to interact with any of its targets
-    fn should_interact(&self, self_info: &<Self::SelfInfo as WorldQuery>::Item<'_>) -> bool;
-
-    /// The number of potential targets to a single interaction.
-    /// i.e. How many unique targets `do_interaction` can return true for before skipping more
-    fn targets_per_interact(&self, self_info: &<Self::SelfInfo as WorldQuery>::Item<'_>) -> usize;
 
     /// Called with all potential `Target`s and their corresponding `TargetInfo`.
     /// This should determine whether `target` can even be targeted by the interaction
     /// and then do whatever the interaction needs to do
-    ///
-    /// Returns whether the interaction happened
-    fn do_interaction(
+    fn interact(
         &mut self,
         self_info: &mut <Self::SelfInfo as WorldQuery>::Item<'_>,
-        target: &mut Target,
-        target_info: &mut <Self::TargetInfo as WorldQuery>::Item<'_>,
-    ) -> bool;
+        target_query: &mut Query<(&mut Target, Self::TargetInfo), Without<Self>>,
+    );
 }
 
 pub fn interaction_system<'a, 'b, A, I, T, S>(
@@ -40,23 +31,6 @@ pub fn interaction_system<'a, 'b, A, I, T, S>(
     T: Component,
 {
     for (mut interactor, mut interactor_info) in &mut interactor_query {
-        if interactor.should_interact(&interactor_info) {
-            let mut interacts_left = interactor.targets_per_interact(&interactor_info);
-            for (mut target, mut target_info) in &mut target_query {
-                if interacts_left == 0 {
-                    break;
-                }
-                let sub = if interactor.do_interaction(
-                    &mut interactor_info,
-                    &mut target,
-                    &mut target_info,
-                ) {
-                    1
-                } else {
-                    0
-                };
-                interacts_left -= sub;
-            }
-        }
+        interactor.interact(&mut interactor_info, &mut target_query);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-/// Things that may change their behavior depending on the presence of a target
-pub mod agent;
 /// Things that may mututally mutably interact
 /// i.e. if a player attacks an enemy the enemy might lose health and the player could gain xp
 pub mod interactions;
